97b2cf33e02ddf356365f7ea225cb0e1bb298075
warextermination <homerokawaii@gmail.com>
2024-03-20 10:37:46 -0600

    Grappling

---
+++ Assets/Enemigos.cs
@@ -0,0 +1,25 @@
+      using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemigos : MonoBehaviour
+{
+    
+    void Start()
+    {
+        private Rigidbody2D rb2D;
+        [SerializeField] private float VelocidaddeMovimiento;
+
+        [SerializeField] private float Distancia;
+
+        [SerializeField] private LayerMask QueesSuelo;
+
+    }
+
+    
+    void Update()
+    {
+        rb2D = Getcomponent<Rigidbody2D>();
+        rb
+    }
+}
